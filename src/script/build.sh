1350b3dc4400be2113dfbc5ed1af36723a08a004
Ian Bytchek <ianbytchek@gmail.com>
2015-09-27 02:32:39 +0400

    Removing golang dependency, apparently binary already contains everything it needs

--- src/script/build.sh
+++ src/script/build.sh
@@ -1,19 +1,12 @@
 #!/usr/bin/env sh
 
-apk update
-apk add go
-
 echo -n 'Moving confd into ´/usr/local/bin´… '
 mv '/docker/confd' '/usr/local/bin'
 echo ' OK!'
 
 echo -n 'Cleaning up container…'
 
-rm -rf \
-    /tmp/* \
-    /var/tmp/* \
-    /var/cache/* \
-    /docker
+rm -rf /docker
 
 echo ' OK!'
 
