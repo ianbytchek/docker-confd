3ebf23470376665e6d7371d74ff857a939fcb441
Ian Bytchek <ianbytchek@gmail.com>
2015-10-06 15:01:52 +0400

    Simplifying build script, adding docker binaries

--- build/ci/script/build.sh
+++ build/ci/script/build.sh
@@ -4,8 +4,23 @@ set -euo pipefail
 
 # Download the latest confd executable and build the docker image.
 
-url=$(curl --header "Authorization: token ${GITHUB_TOKEN}" --silent https://api.github.com/repos/kelseyhightower/confd/releases | grep browser_download_url | grep linux-amd64 | head --lines 1 | cut --delimiter '"' --fields 4)
+echo -n 'Finding condf release url… '
+releases=$(curl --header "Authorization: token ${GITHUB_TOKEN}" --silent https://api.github.com/repos/kelseyhightower/confd/releases)
+url=$(echo "${releases}" | jq --raw-output 'map(select(.prerelease == false and .draft == false))[0].assets | map(select(.name | endswith("linux-amd64")))[0].browser_download_url')
+echo " OK! ${url}"
+
 curl --output './src/confd' --location "$url"
 chmod +x './src/confd'
+
+echo -n 'Finding docker release url… '
+releases=$(curl --header "Authorization: token ${GITHUB_TOKEN}" --silent 'https://api.github.com/repos/docker/docker/releases')
+release=$(echo "${releases}" | jq --raw-output 'map(select(.prerelease == false and .draft == false) | .tag_name)[0][1:]')
+url="https://get.docker.com/builds/Linux/x86_64/docker-${release}.tgz"
+echo " OK! ${url}"
+
+curl --output 'docker.tgz' --location "$url"
+mkdir 'docker'
+tar -zx --directory 'docker' --strip 3 --file 'docker.tgz'
+cp './docker/docker' './src'
 
 docker build --tag "ianbytchek/confd" "./src"
--- src/script/build.sh
+++ src/script/build.sh
@@ -3,13 +3,15 @@
 set -euo pipefail
 
 echo -n 'Moving confd binary into $PATH… '
-mv '/docker/confd' '/usr/local/bin'
+mv '/docker/confd' '/usr/bin'
+echo ' OK!'
+
+echo -n 'Moving docker binary into $PATH… '
+mv '/docker/confd' '/usr/bin'
 echo ' OK!'
 
 echo -n 'Cleaning up container…'
-
 rm -rf /docker
-
 echo ' OK!'
 
 echo 'Bye-bye!'
